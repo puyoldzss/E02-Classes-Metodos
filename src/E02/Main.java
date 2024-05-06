e67f722c1124c9290418ced82327138046eee4c7
puyoldzss <a2023950982@teiacoltec.org>
2024-05-06 08:13:29 -0300

    E02

---
+++ src/E02/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        Conta objA = new Conta();
+
+        objA.dono = "Pudjol";
+        objA.saldo = 2500;
+        objA.agencia = "120-9";
+        objA.numero = 16;
+
+        objA.imprimir();
+    }
+}
