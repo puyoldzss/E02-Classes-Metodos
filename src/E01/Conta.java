46345155ff3c516f8ccd65032e34e910105869fb
puyoldzss <a2023950982@teiacoltec.org>
2024-05-06 08:08:37 -0300

    E01

---
+++ src/E01/Conta.java
@@ -0,0 +1,29 @@
+public class Conta{
+    int numero;
+    double saldo;
+    String dono;
+    String agencia;
+
+    boolean deposito(double valor){
+        if (valor > 0){
+            this.saldo += valor;
+            return true;
+        }
+        return false;
+    }
+    boolean saque(double valor){
+        if (valor > 0 && valor <= this.saldo){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    boolean transferir(Conta destino, double valor){
+        boolean saque = this.saque(valor);
+        if (saque){
+            boolean deposito = destino.deposito(valor);
+            return deposito;
+        }
+        return false;
+    }
+}
